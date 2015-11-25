5c929aab5b07a53bf127fcb1bf53b089ef41ebac
Markus Hjort <markus.hjort@gmail.com>
2015-11-25 14:23:36 +0200

    Process messages when they arrive

--- src/lambda_demo/core.clj
+++ src/lambda_demo/core.clj
@@ -1,9 +1,11 @@
 (ns lambda-demo.core
-  (:require [uswitch.lambada.core :refer [deflambdafn]]
+  (:require [lambda-demo.handler :refer [process]]
+            [uswitch.lambada.core :refer [deflambdafn]]
             [clojure.java.io :as io]
             [cheshire.core :refer [parse-stream]]))
 
 (deflambdafn lambda-demo.LambdaFn
   [in out ctx]
   (let [input (parse-stream (io/reader in) true)]
-    (println "Hello from Lambda with input" input)))
+    (println "Hello from Lambda with input" input)
+    (process input)))
