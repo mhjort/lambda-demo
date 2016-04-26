ef220402cae315f94f0aff322572663cd877db2a
Markus Hjort <markus.hjort@gmail.com>
2016-04-26 14:09:15 +0300

    Use lein-clj-lambda plugin for deployments

--- project.clj
+++ project.clj
@@ -8,6 +8,14 @@
                  [base64-clj "0.1.1"]
                  [uswitch/lambada "0.1.0"]
                  [com.amazonaws/aws-java-sdk-s3 "1.10.37"]]
+  :plugins [[lein-clj-lambda "0.3.0"]]
+  :lambda {"test" [{:function-name "LambdaDemo"
+                    :handler "lambda-demo.LambdaFn"
+                    :memory-size 1024
+                    :timeout 300
+                    :region "eu-west-1"
+                    :s3 {:bucket "markus-lambda-demo"
+                         :object-key "lambda.jar"}}]}
   :profiles {:dev
              {:dependencies [[amazonica "0.3.39"
                               :exclusions [com.amazonaws/aws-java-sdk]]
