305a152d973d8b63fe04a4868f0e6ce594e8df28
Markus Hjort <markus.hjort@gmail.com>
2016-04-26 14:09:31 +0300

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mhjort/lambda-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]
                  [base64-clj "0.1.1"]
                  [uswitch/lambada "0.1.0"]
